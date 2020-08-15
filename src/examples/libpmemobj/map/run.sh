4381d8dbebd682fab7103d792f950b7cf5309c0a
Hamed Gorjiara <hgorjiar@uci.edu>
2020-08-14 21:47:34 -0700

    Edit

--- src/examples/libpmemobj/map/run.sh
+++ src/examples/libpmemobj/map/run.sh
@@ -3,6 +3,6 @@ export LD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/
 # For Mac OSX
 export DYLD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/
 export PMCheck="-p 1 -v 3"
-echo ""
+echo "./run.sh ./data_store <ctree|btree|rbtree|hashmap_atomic|hashmap_tx> ./path/to/file [number of inserts]"
 echo $@
 $@
