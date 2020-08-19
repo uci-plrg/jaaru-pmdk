19d474d5094980896f5417174199308a421fdfbe
Hamed Gorjiara <hgorjiar@uci.edu>
2020-08-18 18:56:48 -0700

    Adding -d option to run.sh

--- src/examples/libpmemobj/map/run.sh
+++ src/examples/libpmemobj/map/run.sh
@@ -2,7 +2,7 @@
 export LD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/
 # For Mac OSX
 export DYLD_LIBRARY_PATH=/scratch/nvm/pmcheck/bin/
-export PMCheck="-p2 -v3"
+export PMCheck="-d$3 -c0 -p1 -v3"
 echo "./run.sh ./data_store <ctree|btree|rbtree|hashmap_atomic|hashmap_tx> ./path/to/file [number of inserts]"
 echo $@
 $@
